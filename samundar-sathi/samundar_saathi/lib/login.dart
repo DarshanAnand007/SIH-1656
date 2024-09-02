9d268d5f18a298e6f7dd8e85f782581e33e4111c
Naindeep-Singh <naindeep090@gmail.com>
2024-09-02 18:28:16 +0530

    feat: structure

---
+++ samundar-sathi/samundar_saathi/lib/login.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:samundar_saathi/homepage.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomePage(),
+                  ),
+                );
+              },
+              child: const Text('Go to homepage'))
+        ],
+      ),
+    );
+  }
+}
